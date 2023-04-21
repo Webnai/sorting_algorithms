efce811ba0b7c41f98bde97421d184d191199c7e
Webnai <wilbertboadzo144@gmail.com>
2023-04-21 12:54:03 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,23 +8,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
+    unsigned int i, j;
+    int tmp;
+    int swapped;
 
-	if (size < 2)
-		return;
+    if (size < 2)
+        return;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
-			}
-		}
-	}
+    for (i = 0; i < size; i++)
+    {
+        swapped = 0; // Initialize swapped flag to 0
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size);
+                swapped = 1; // Set swapped flag to 1 if elements are swapped
+            }
+        }
+
+        if (swapped == 0)
+            break; // If no elements are swapped in the inner loop, the array is already sorted
+    }
 }
+

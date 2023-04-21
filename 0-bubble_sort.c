c0efdeadb72dbbabeeb7e11ae8e59f4b14481179
Webnai <wilbertboadzo144@gmail.com>
2023-04-21 12:58:25 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * bubble_sort - sorts an array in ascending order
+ * bubble_sort - sorts an array in ascending order using bubble sort
  * @array: array of ints to sort
  * @size: size of the array
  */
@@ -10,14 +10,12 @@ void bubble_sort(int *array, size_t size)
 {
     unsigned int i, j;
     int tmp;
-    int swapped;
 
     if (size < 2)
         return;
 
     for (i = 0; i < size; i++)
     {
-        swapped = 0; // Initialize swapped flag to 0
         for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
@@ -26,12 +24,26 @@ void bubble_sort(int *array, size_t size)
                 array[j] = array[j + 1];
                 array[j + 1] = tmp;
                 print_array(array, size);
-                swapped = 1; // Set swapped flag to 1 if elements are swapped
             }
         }
-
-        if (swapped == 0)
-            break; // If no elements are swapped in the inner loop, the array is already sorted
     }
+}
+
+/**
+ * print_array - prints the elements of an array
+ * @array: array of ints to print
+ * @size: size of the array
+ */
+void print_array(const int *array, size_t size)
+{
+    unsigned int i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+        if (i < size - 1)
+            printf(", ");
+    }
+    printf("\n");
 }
 

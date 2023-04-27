331cfe76c0cfdf4be50c48e9a1f12c84c425c4a9
Webnai <wilbertboadzo144@gmail.com>
2023-04-27 03:14:45 -0700

    counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,46 +1,59 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using Counting sort algorithm
+ * get_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
  */
-void counting_sort(int *array, size_t size)
+int get_max(int *array, size_t size)
 {
-    int *count, *sorted, max, i;
+    int max = array[0];
+    size_t i;
 
-    if (array == NULL || size < 2)
-        return;
-
-    sorted = malloc(sizeof(int) * size);
-    if (sorted == NULL)
-        return;
-
-    max = array[0];
-    for (i = 1; i < (int)size; i++)
+    for (i = 1; i < size; i++)
     {
         if (array[i] > max)
             max = array[i];
     }
 
-    count = malloc(sizeof(int) * (max + 1));
-    if (count == NULL)
+    return max;
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ * using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
+void counting_sort(int *array, size_t size)
+{
+    int *count, *sorted;
+    int max, i;
+
+    if (!array || size < 2)
+        return;
+
+    sorted = malloc(sizeof(int) * size);
+    if (!sorted)
+        return;
+
+    max = get_max(array, size);
+    count = calloc(max + 1, sizeof(int));
+    if (!count)
     {
         free(sorted);
         return;
     }
 
-    for (i = 0; i < (max + 1); i++)
-        count[i] = 0;
-
-    for (i = 0; i < (int)size; i++)
+    for (i = 0; i < size; i++)
         count[array[i]]++;
-
-    printf("Counting array:");
-    print_array(count, max + 1);
-
     for (i = 1; i <= max; i++)
         count[i] += count[i - 1];
+    print_array(count, max + 1);
 
     for (i = size - 1; i >= 0; i--)
     {
@@ -48,9 +61,9 @@ void counting_sort(int *array, size_t size)
         count[array[i]]--;
     }
 
-    for (i = 0; i < (int)size; i++)
+    for (i = 0; i < size; i++)
         array[i] = sorted[i];
 
-    free(count);
     free(sorted);
+    free(count);
 }
